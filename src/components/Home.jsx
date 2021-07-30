4bb1c0da5e605dab289093cae4261c364e44097a
diego_lima <lima.j.diego@gmail.com>
2021-07-30 10:20:49 -0300

    component home

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import State from '../hooks/State'
+import Effect from '../hooks/Effect'
+
+export default ()=>{
+    return(
+        <>
+            <h1>Estudando hooks</h1>
+            <Effect/>           
+        </>
+    )
+}
